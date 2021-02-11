eb9f6def2971954af22f801b857b3583b562ef75
Ali Shah <s.ashah@outlook.com>
2021-02-11 10:17:38 -0500

    [Finished] routes.py

--- events_app/routes.py
+++ events_app/routes.py
@@ -16,65 +16,68 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     """Show upcoming events to users!"""
-    # TODO: Get all events and send to the template
-    return render_template('index.html')
-
+    events = Event.query.all()
+    return render_template('index.html', events=events)
 
 @main.route('/event/<event_id>', methods=['GET'])
 def event_detail(event_id):
     """Show a single event."""
-    # TODO: Get the event with the given id and send to the template
-    return render_template('event_detail.html')
-
+    event = Event.query.filter_by(id=event_id).one()
+    return render_template('event_detail.html', event=event)
 
 @main.route('/event/<event_id>', methods=['POST'])
 def rsvp(event_id):
     """RSVP to an event."""
-    # TODO: Get the event with the given id from the database
     is_returning_guest = request.form.get('returning')
     guest_name = request.form.get('guest_name')
 
+    event = Event.query.filter_by(id=event_id).one()
+
     if is_returning_guest:
-        # TODO: Look up the guest by name, and add the event to their 
-        # events_attending, then commit to the database
-        pass
+        guest = Guest.query.filter_by(name=guest_name).one()
+        guest.events.append(event)
+
+        db.session.add(guest)
+        db.session.commit()
     else:
         guest_email = request.form.get('email')
         guest_phone = request.form.get('phone')
-        # TODO: Create a new guest with the given name, email, and phone, and 
-        # add the event to their events_attending, then commit to the database
-        pass
-    
-    flash('You have successfully RSVP\'d! See you there!')
-    return redirect(url_for('main.event_detail', event_id=event_id))
 
+        guest = Guest(name=guest_name, email=guest_email, phone=guest_phone)
+        guest.events.append(event)
+
+        db.session.add(guest)
+        db.session.commit()
+
+    flash("You have successfully RSVP'd! See you there!")
+    return redirect(url_for('main.event_detail', event_id=event_id))
 
 @main.route('/create', methods=['GET', 'POST'])
 def create():
     """Create a new event."""
-    if request.method == 'POST':
-        new_event_title = request.form.get('title')
-        new_event_description = request.form.get('description')
-        date = request.form.get('date')
-        time = request.form.get('time')
-
-        try:
-            date_and_time = datetime.strptime(
-                f'{date} {time}',
-                '%Y-%m-%d %H:%M')
-        except ValueError:
-            print('there was an error: incorrect datetime format')
-
-        # TODO: Create a new event with the given title, description, & 
-        # datetime, then add and commit to the database
-
-        flash('Event created.')
-        return redirect(url_for('main.index'))
-    else:
+    if request.method == 'GET':
         return render_template('create.html')
 
+    title = request.form.get('title')
+    description = request.form.get('description')
+    date = request.form.get('date')
+    time = request.form.get('time')
+
+    try:
+        date_and_time = datetime.strptime(f'{date} {time}', '%Y-%m-%d %H:%M')
+    except ValueError:
+        print('there was an error: incorrect datetime format')
+
+    new_event = Event(title=title, description=description, date_and_time=date_and_time)
+    db.session.add(new_event)
+    db.session.commit()
+
+    flash("Event created.")
+
+    return redirect(url_for('main.index'))
 
 @main.route('/guest/<guest_id>')
 def guest_detail(guest_id):
-    # TODO: Get the guest with the given id and send to the template
-    return render_template('guest_detail.html')
+    """Show a single guest."""
+    guest = Guest.query.filter_by(id=guest_id).one()
+    return render_template('guest_detail.html', guest=guest)
